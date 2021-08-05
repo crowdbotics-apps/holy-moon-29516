e4622131af38af53b59839b7984abcddd4457075
Crowdbotics <team@crowdbotics.com>
2021-08-05 16:27:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "holy_moon_29516Identifier",
+  url: "https://holy_moon_29516Identifier.botics.co",
+  api: "https://holy_moon_29516Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
